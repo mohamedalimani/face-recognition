f9e85cd20726cfb4b4f9e97fd6ca9c2c26090f1b
Manix <52538269+mohamedalimani@users.noreply.github.com>
2020-05-01 00:07:27 +0100

    Update addGUI.py

--- pfe/code/addGUI.py
+++ pfe/code/addGUI.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'reconGUI2.ui'
-#
-# Created by: PyQt5 UI code generator 5.10.1
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-import reconGUI, os, time, cv2, sys
+import reconGUI, os, time, cv2, sys, pickle
+from PIL import Image
+import numpy as np
 
 
 class addDataThread(QtCore.QThread):
@@ -23,6 +17,7 @@ class addDataThread(QtCore.QThread):
         (self.im_width, self.im_height) = (110, 90)
         self.size = 1
         self.lbp_cascade_face = cv2.CascadeClassifier(f'Data/{self.lbp_classifier_face}')
+        self.count_max = 40
 
     def run(self):
         self.camera = cv2.VideoCapture(0)
@@ -32,8 +27,8 @@ class addDataThread(QtCore.QThread):
         count = 0
         pause = 0
         t = 0
-        count_max = 40
-        while count < count_max:
+        # count_max = 40
+        while count < self.count_max:
             (working, frame) = self.camera.read()
             # Loop until the camera is working
             while not working:
@@ -62,14 +57,14 @@ class addDataThread(QtCore.QThread):
                 face_resize = cv2.resize(face, (self.im_width, self.im_height))
                 # Draw rectangle and write name
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-                cv2.putText(frame, '%s - %d/40' % (self.target_name, count), (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN,1, (0, 255, 0))
+                cv2.putText(frame, '%s - %d/%d' % (self.target_name, count, self.count_max), (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN,1, (0, 255, 0))
                 # Remove false positives
                 if w * 6 < width or h * 6 < height:
                     print("Non claire")
                 else:
                     # To create diversity, only save every fith detected image
                     if pause == 0:
-                        print("enregistrement de la capture " + str(count + 1) + "/" + str(count_max))
+                        print("enregistrement de la capture " + str(count + 1) + "/" + str(self.count_max))
 
                         # Save image file
                         cv2.imwrite('%s/%s.png' % (self.path, pin), face_resize)
@@ -97,7 +92,7 @@ class Ui_addnewface(object):
 
     def setupUi(self, addnewface):
         addnewface.setObjectName("addnewface")
-        addnewface.resize(864, 600)
+        addnewface.resize(863, 600)
         self.centralwidget = QtWidgets.QWidget(addnewface)
         self.centralwidget.setObjectName("centralwidget")
         self.addDatascreen = QtWidgets.QGraphicsView(self.centralwidget)
@@ -118,24 +113,82 @@ class Ui_addnewface(object):
         self.label.setObjectName("label")
         self.addDatastart = QtWidgets.QPushButton(self.centralwidget)
         self.addDatastart.setGeometry(QtCore.QRect(8, 140, 121, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman L")
+        font.setPointSize(13)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.addDatastart.setFont(font)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("2.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.addDatastart.setIcon(icon)
+        self.addDatastart.setIconSize(QtCore.QSize(52, 26))
         self.addDatastart.setObjectName("addDatastart")
+        self.imagesNumber = QtWidgets.QSlider(self.centralwidget)
+        self.imagesNumber.setGeometry(QtCore.QRect(10, 230, 201, 16))
+        self.imagesNumber.setMinimum(10)
+        self.imagesNumber.setMaximum(100)
+        self.imagesNumber.setSingleStep(10)
+        self.imagesNumber.setSliderPosition(40)
+        self.imagesNumber.setOrientation(QtCore.Qt.Horizontal)
+        self.imagesNumber.setObjectName("imagesNumber")
+        self.imgNumLabel = QtWidgets.QLabel(self.centralwidget)
+        self.imgNumLabel.setGeometry(QtCore.QRect(10, 190, 151, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.imgNumLabel.setFont(font)
+        self.imgNumLabel.setObjectName("imgNumLabel")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(70, 250, 31, 21))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(180, 250, 41, 20))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(10, 290, 141, 17))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.train_faces_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.train_faces_btn.setGeometry(QtCore.QRect(10, 330, 131, 41))
+        font = QtGui.QFont()
+        font.setFamily("Ubuntu Condensed")
+        font.setPointSize(13)
+        font.setBold(True)
+        font.setWeight(75)
+        self.train_faces_btn.setFont(font)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("1.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.train_faces_btn.setIcon(icon1)
+        self.train_faces_btn.setIconSize(QtCore.QSize(29, 30))
+        self.train_faces_btn.setObjectName("train_faces_btn")
         addnewface.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(addnewface)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 864, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 863, 22))
         self.menubar.setObjectName("menubar")
         self.menunavigate = QtWidgets.QMenu(self.menubar)
         self.menunavigate.setObjectName("menunavigate")
+        self.menuoption = QtWidgets.QMenu(self.menubar)
+        self.menuoption.setObjectName("menuoption")
         addnewface.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(addnewface)
         self.statusbar.setObjectName("statusbar")
         addnewface.setStatusBar(self.statusbar)
         self.toreconGUI = QtWidgets.QAction(addnewface)
         self.toreconGUI.setObjectName("toreconGUI")
+        self.actionarret_forc = QtWidgets.QAction(addnewface)
+        self.actionarret_forc.setObjectName("actionarret_forc")
         self.menunavigate.addAction(self.toreconGUI)
+        self.menuoption.addAction(self.actionarret_forc)
         self.menubar.addAction(self.menunavigate.menuAction())
+        self.menubar.addAction(self.menuoption.menuAction())
         self.toreconGUI.triggered.connect(self.showWindow1)
         self.addDatascreen.setScene(self.scene)
         self.addDataThread.frameItem.connect(self.scene.addItem)
+        self.actionarret_forc.triggered.connect(self.force_exit)
+        self.train_faces_btn.clicked.connect(self.train_data)
 
         self.retranslateUi(addnewface)
         QtCore.QMetaObject.connectSlotsByName(addnewface)
@@ -143,11 +196,24 @@ class Ui_addnewface(object):
     def retranslateUi(self, addnewface):
         _translate = QtCore.QCoreApplication.translate
         addnewface.setWindowTitle(_translate("addnewface", "add Data"))
+        self.addDatascreen.setStatusTip(_translate("addnewface", "déplacer votre tete au milieu de l\'ecran"))
         self.label.setText(_translate("addnewface", "tapez le nom du personnage :"))
-        self.addDatastart.setText(_translate("addnewface", "start"))
+        self.addDatastart.setStatusTip(_translate("addnewface", "ajouter des photo au base de donnée"))
+        self.addDatastart.setText(_translate("addnewface", "  start"))
+        self.addDatastart.setShortcut(_translate("addnewface", "Return"))
+        self.imagesNumber.setStatusTip(_translate("addnewface", "choisir le nombre des images "))
+        self.imgNumLabel.setText(_translate("addnewface", "nombre des photo"))
+        self.label_3.setText(_translate("addnewface", "x40"))
+        self.label_4.setText(_translate("addnewface", "x100"))
+        self.label_5.setText(_translate("addnewface", "entrainer l\'AI :"))
+        self.train_faces_btn.setStatusTip(_translate("addnewface", "entrainer l\'AI a reconnaiser les personnes que tu as ajouté"))
+        self.train_faces_btn.setText(_translate("addnewface", "    entraine"))
         self.menunavigate.setTitle(_translate("addnewface", "navigate"))
+        self.menuoption.setTitle(_translate("addnewface", "option"))
         self.toreconGUI.setText(_translate("addnewface", "vers reconnaissance facial"))
         self.toreconGUI.setStatusTip(_translate("addnewface", "ouvrir la page de réconnaissance faciale"))
+        self.actionarret_forc.setText(_translate("addnewface", "arret forcé (ctrl+E)"))
+        self.actionarret_forc.setShortcut(_translate("addnewface", "Ctrl+E"))
 
     def showWindow1(self):
         self.window = QtWidgets.QMainWindow()
@@ -166,6 +232,8 @@ class Ui_addnewface(object):
                 print(time_passed - i)
 
     def add_data(self):
+        self.scene.clear()
+        self.addDataThread.count_max = self.imagesNumber.value()
         msg = QtWidgets.QWidget()
         # create file and label it or find it if it already exists
         if len(self.addDataLabel.text()) == 0:
@@ -184,5 +252,64 @@ class Ui_addnewface(object):
 
             self.addDataLabel.clear()
             self.addDataThread.start()
+
+    def force_exit(self):
+        msg = QtWidgets.QMessageBox()
+        is_exit = msg.warning(QtWidgets.QWidget(), "attention !", "étes vous sure d'arreter le programme !", msg.Yes | msg.No, msg.No)
+        if is_exit == msg.Yes:
+            exit()
+        elif is_exit == msg.No:
+            pass
+
+    def train_data(self):
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        image_dir = os.path.join(BASE_DIR, "../target/")
+        face_cascade = cv2.CascadeClassifier("Data/haarcascade_frontalface_default.xml")
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+        current_id = 0
+        label_ids = {}
+        y_labels = []
+        x_train = []
+
+        for root, dirs, files in os.walk(image_dir):
+            for file in files:
+                if file.endswith("png") or file.endswith("jpg"):
+                    path = os.path.join(root, file)
+                    label = os.path.basename(os.path.dirname(path)).replace(' ', '-').lower()
+                    if not label in label_ids:
+                        label_ids[label] = current_id
+                        current_id += 1
+                    id_ = label_ids[label]
+                    pil_image = Image.open(path).convert("L")  # convert image into gray
+                    size = (550, 550)
+                    final_image = pil_image.resize(size, Image.ANTIALIAS)
+                    image_array = np.array(final_image, "uint8")
+                    faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.1, minNeighbors=5,
+                                                          minSize=(30, 30))
+                    for (x, y, w, h) in faces:
+                        roi = image_array[y:y + h, x:x + w]
+                        x_train.append(roi)
+                        y_labels.append(id_)
+
+        with open("Data/labels.pickle", 'wb') as f:
+            pickle.dump(label_ids, f)
+
+        recognizer.train(x_train, np.array(y_labels))
+        recognizer.save("Data/trainer.yml")
+        msg = QtWidgets.QMessageBox()
+        msg.information(QtWidgets.QWidget(), "success !", "l'entrainement est terminé avec succées", msg.Ok)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
